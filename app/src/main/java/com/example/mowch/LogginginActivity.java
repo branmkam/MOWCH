83f008ffce5ee2cb96200403384a62238b51c267
vpatel99 <vivekpatel1999.vdp@gmail.com>
2020-07-03 11:39:38 -0400

    General bug fixes on Forgot Password screen
    Signed-off-by: vpatel99 <vivekpatel1999.vdp@gmail.com>

--- app/src/main/java/com/example/mowch/LogginginActivity.java
+++ app/src/main/java/com/example/mowch/LogginginActivity.java
@@ -3,12 +3,30 @@ package com.example.mowch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class LogginginActivity extends AppCompatActivity {
+
+    Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loggingin);
+
+        signup = (Button) findViewById(R.id.signup_button);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        }
+
+
+
     }
-}
