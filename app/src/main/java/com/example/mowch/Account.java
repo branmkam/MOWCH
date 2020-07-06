9a4c28c0956fca481dfcada88994c00e63f6cf1b
Charlotte Wang <charlottewang2000@gmail.com>
2020-07-06 16:41:13 -0400

    Deleted username and number from account properties

--- app/src/main/java/com/example/mowch/Account.java
+++ app/src/main/java/com/example/mowch/Account.java
@@ -2,15 +2,11 @@ package com.example.mowch;
 
 public class Account {
     protected String name;
-    protected int number;
-    protected String username;
     protected String email;
     protected String password;
 
-    public Account(String name, int number, String username, String email, String password) {
+    public Account(String name, String email, String password) {
         this.name = name;
-        this.number = number;
-        this.username = username;
         this.email = email;
         this.password = password;
     }
@@ -22,22 +18,6 @@ public class Account {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public int getNumber() {
-        return number;
-    }
-
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 
     public String getEmail() {
