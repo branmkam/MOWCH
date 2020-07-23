2bac54d58794d1b2b20fba56c09b86ab412b0613
Brandon Kaminski <54331071+branmkam@users.noreply.github.com>
2020-07-23 12:11:50 -0400

    Create TestData.java

---
+++ app/src/main/java/com/example/mowch/TestData.java
@@ -0,0 +1,37 @@
+package com.example.mowch;
+import java.util.*;
+
+public class TestData {
+
+    public void createFields() {
+        Admin rb = new Admin("Rachel Bearman", "rachelbearman@cchmow.org", "rAcHeL20";
+
+        Driver d1 = new Driver("Brandon Kaminski", "branmkam@live.unc.edu", "d1pwd", rb);
+        Driver d2 = new Driver("John Smith", "johnsmith@example.net", "d2pwd", rb);
+        Address a1 = new Address("Charlotte", "Wang", "100 E Franklin Street", "Chapel Hill", false, "no pork");
+        Address a2 = new Address("Nada", "Rahmouni", "200 E Franklin Street", "Chapel Hill", false, "");
+        Address a3 = new Address("Vivek", "Patel", "300 E Franklin Street", "Chapel Hill", false, "no peanuts");
+        Address a4 = new Address("Tatiana", "Kelliher", "400 E Franklin Street", "Chapel Hill", false, "");
+        Address a5 = new Address("Maya", "Agnihotri", "500 E Franklin Street", "Chapel Hill", false, "");
+
+        ArrayList<Address> AL1 = new ArrayList<Address>();
+        AL1.add(a1);
+        AL1.add(a2);
+
+        ArrayList<Address> AL2 = new ArrayList<Address>();
+        AL1.add(a3);
+        AL1.add(a4);
+        AL1.add(a5);
+
+        Route r1 = new Route(1);
+        r1.setAddresses(AL1);
+        rb.assignRoute(d1, r1);
+
+        Route r2 = new Route(2);
+        r2.setAddresses(AL2);
+
+        d1.setRoute(r1);
+        d2.setRoute(r2);
+    }
+
+}
