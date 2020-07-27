cae8602406e9f91d43c4a036360df132087cadbe
apple <merielt@live.unc.edu>
2020-07-27 00:40:17 -0500

    added change password pop up

---
+++ app/src/main/java/com/example/mowch/ChangingPasswordPop.java
@@ -0,0 +1,14 @@
+package com.example.mowch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangingPasswordPop extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_changing_password_pop);
+    }
+}
