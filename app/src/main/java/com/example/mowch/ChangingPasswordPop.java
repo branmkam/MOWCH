4d2f1653f2adfac8be61d1c1894bb5842925fddf
apple <merielt@live.unc.edu>
2020-07-27 22:25:47 -0500

    Added better padlock button, connected change email and change password pages to settings page

--- app/src/main/java/com/example/mowch/ChangingPasswordPop.java
+++ app/src/main/java/com/example/mowch/ChangingPasswordPop.java
@@ -1,14 +1,57 @@
 package com.example.mowch;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class ChangingPasswordPop extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_changing_password_pop);
+
+
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        int width = displayMetrics.widthPixels;
+        int height = displayMetrics.heightPixels;
+
+        getWindow().setLayout((int) (width*.85), (int) (height*.60));
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.CENTER;
+        params.x = 0;
+        params.y = 0;
+        getWindow().setAttributes(params);
+
+        Button pause = (Button) findViewById(R.id.confirmchange);
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openOtherPopup();
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    private void openOtherPopup() {
+        finish();
+        Intent intent = new Intent(this, SettingsFragment.class);
+        startActivity(intent);
     }
 }
