ceb6475e448327b4fdd934f9e84a8bd0d68d62a6
branmkam <54331071+branmkam@users.noreply.github.com>
2020-07-29 12:39:36 -0400

    TOKEN GOTTEN WITH REQUEST

--- app/src/main/java/com/example/mowch/APIGetTest.java
+++ app/src/main/java/com/example/mowch/APIGetTest.java
@@ -8,23 +8,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.android.volley.AuthFailureError;
 import com.android.volley.Cache;
 import com.android.volley.Network;
-import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.DiskBasedCache;
-import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -35,10 +31,10 @@ public class APIGetTest extends AppCompatActivity {
     //final TextView textView = new TextView(this);
     //env vars
     final String server = "dev-fms18-01.soliantconsulting.com";
-    final String file = "MOW_DataAPI.fmp12";
+    final String file = "MealsOnWheels.fmp12";
     final String layout = "MOW_DataAPI";
-    final String username = "";
-    final String pw = "";
+    final String username = "dataapi";
+    final String pw = "MOWSoliant";
 
     public void getTestRequest(String url) {
         RequestQueue requestQueue;
@@ -66,7 +62,7 @@ public class APIGetTest extends AppCompatActivity {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        System.out.println("Error error you're dumb");
+                        System.out.println("Error error");
                         System.out.println(error.toString());
                     }
                 });
@@ -78,17 +74,15 @@ public class APIGetTest extends AppCompatActivity {
 
     public void authorizeMOWCH(String url)
     {
-        try {
             RequestQueue requestQueue = Volley.newRequestQueue(this);
-            JSONObject jsonBody = new JSONObject();
-            jsonBody.put("MOWCH", "Demo");
-            jsonBody.put("Author", "BMK");
-            final String requestBody = jsonBody.toString();
+//            JSONObject jsonBody = new JSONObject();
+//            jsonBody.put("MOWCH", "Demo");
+//            jsonBody.put("Author", "BMK");
+//            final String requestBody = jsonBody.toString();
 
-            StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    System.out.println("received object!!!!");
+            JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
+                public void onResponse(JSONObject response) {
+                    System.out.println(response.toString());
                 }
             }, new Response.ErrorListener() {
                 @Override
@@ -101,26 +95,26 @@ public class APIGetTest extends AppCompatActivity {
                     return "application/json";
                 }
 
-                @Override
-                public byte[] getBody() throws AuthFailureError {
-                    try {
-                        return requestBody == null ? null : requestBody.getBytes("utf-8");
-                    } catch (UnsupportedEncodingException uee) {
-                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
-                        return null;
-                    }
-                }
+//                @Override
+//                public byte[] getBody() throws AuthFailureError {
+//                    try {
+//                        return requestBody == null ? null : requestBody.getBytes("utf-8");
+//                    } catch (UnsupportedEncodingException uee) {
+//                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+//                        return null;
+//                    }
+//                }
 
-                @Override
-                protected Response<String> parseNetworkResponse(NetworkResponse response) {
-                    String responseString = "";
-                    if (response != null) {
-                        responseString = String.valueOf(response.statusCode);
-                        // can get more details such as response.headers
-                        System.out.println("responseString: " + responseString);
-                    }
-                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
-                }
+//                @Override
+//                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+//                    String responseString = "";
+//                    if (response != null) {
+//                        responseString = String.valueOf(response.statusCode);
+//                        // can get more details such as response.headers
+//                        System.out.println("responseString: " + responseString);
+//                    }
+//                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+//                }
 
                 @Override
                 public Map<String, String> getHeaders() throws AuthFailureError {
@@ -130,7 +124,7 @@ public class APIGetTest extends AppCompatActivity {
                         String auth = "Basic " + Base64.encodeToString(creds.getBytes(), Base64.NO_WRAP);
                         headers.put("Authorization", auth);
                         headers.put("Content-type","application/json");
-                        headers.put("Accept","application/json");
+                        //headers.put("Accept","application/json");
                         return headers;
                     }
                     catch (Exception e)
@@ -141,11 +135,9 @@ public class APIGetTest extends AppCompatActivity {
                     return headers;
                 }
             };
-
+    //query delivery day, loop through each id _find?ID=int
+        //portal data in DRN
             requestQueue.add(stringRequest);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
     }
 
     protected void onCreate(Bundle savedInstanceState){
